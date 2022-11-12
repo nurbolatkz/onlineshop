dff5ed904be11364e6e57b2a66e9a0d2a8a01666
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2022-11-12 18:41:53 +0600

    fix some errors

--- product/models.py
+++ product/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from shop.models import Shop
 # Create your models here.
+
+
 class ProductCategory(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200, null=True, blank=True)
--- shop/models.py
+++ shop/models.py
@@ -21,3 +21,4 @@ class Shop(models.Model):
 
     def __str__(self):
         return self.name
+        
